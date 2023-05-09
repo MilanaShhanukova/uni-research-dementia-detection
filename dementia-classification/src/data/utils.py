f28933003253363f80077be58df12c2396067cbb
Milana <Milana@MacBook-Air.local>
2023-05-09 18:48:52 +0300

    text models modifications

--- dementia-classification/src/data/utils.py
+++ dementia-classification/src/data/utils.py
@@ -1,4 +1,6 @@
+import json
 import os
+import pandas as pd
 import glob
 import subprocess
 from hyperpyyaml import load_hyperpyyaml
@@ -19,6 +21,13 @@ def runcmd(cmd, verbose = False, *args, **kwargs):
     if verbose:
         print(std_out.strip(), std_err)
 
+
+def get_task(file_name):
+    if "cookie" in file_name: return "CPD"
+    elif "recall" in file_name: return "Story Recall"
+    return "Conversation"
+
+
 def load_config(config_path: str) -> dict:
     """
     Load configuration file in yaml format.
@@ -29,7 +38,6 @@ def load_config(config_path: str) -> dict:
         return {}
     with open(config_path, encoding='utf-8') as f:
         return load_hyperpyyaml(f)
-
 
 def get_files_names(main_dir, ext):
     """
@@ -57,3 +65,21 @@ def get_file_name(full_path: str, local_dir: str):
         return file_name
     except IndexError:
         return full_path
+
+def filter_data(data_path):
+    data = pd.read_csv(data_path)
+    data['mode'] = data['audio_paths'].apply(lambda x: x.split('_')[-1][:-4])
+    data = data.groupby('mode').agg({'silence_nums': 'mean', 'percent_silence': 'mean'})
+    return data
+
+def change_holland_info(json_path, seconds_info):
+    with open(json_path) as f:
+        data = json.load(f)
+    for name, info in data.items():
+        for utterance_seconds in info[1]:
+            utterance_seconds['seconds'][0] -= seconds_info[name] * 1000
+            utterance_seconds['seconds'][1] -= seconds_info[name] * 1000
+
+    with open(json_path[:-5] + '_new.json', 'w') as f:
+        json.dump(data, f)
+
